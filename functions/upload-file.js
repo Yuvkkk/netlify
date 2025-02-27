39ceb88fb1db325ba93913c3a29844c0bb0d2d74
Yuvkkk <732609098@qq.com>
2025-02-28 01:10:47 +0800

    Update upload-file.js

--- functions/upload-file.js
+++ functions/upload-file.js
@@ -4,12 +4,16 @@ const accountId = "005fa8f08ff41590000000002";
 const applicationKey = "K005rTY6c7IuYqYDDdYQbhlCEc9qy3Y";
 const authUrl = "https://api.backblazeb2.com/b2api/v2/b2_authorize_account";
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  console.log("收到请求:", event.body ? event.body.slice(0, 100) : "无请求体");
   try {
     const response = await axios.post(authUrl, null, {
       auth: {
         username: accountId,
         password: applicationKey,
+      },
+      headers: {
+        "Content-Type": "application/json", // 明确指定，避免默认行为
       },
     });
 
